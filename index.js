71d48aae0b61abfc651502c23d4caeed4aef036e
Harrison Stephan <harrisonstephan@Harrisons-iMac.local>
2024-01-18 15:57:48 -0700

    added the index.js code

--- index.js
+++ index.js
@@ -1,13 +1,71 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const path = require("path");
+const generateMarkdown = require("generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "github",
+    message: "What is your Github username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "Title:",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Description",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Installation",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Usage Information:",
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "Contibuting:",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Test:",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "License Type:",
+    choices: ["MIT", "APACHE", "GPL", "None"]
+  }, 
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), filename), data)
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((inquirerResponses) => {
+    console.log("Generating README...");
+    writeToFile("README.md", generateMarkdown({...inquirerResponses}));
+    })
+}
 
 // Function call to initialize app
 init();
